8fc9bd21f5ca8fb05a11f96528575451ef29c44e
Victor Williams <victor.williams@stu.cu.edu.ng>
2023-04-29 23:10:10 +0100

    new commit

---
+++ binomial.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void main() {
+  int n;
+  printf("enter power: ");
+  scanf("%d", &n);
+  printf("\n");
+
+  for (int i = 1; i <= n; i++) {
+    int result = commutation(n, i);
+    printf("%dx^%dy^%d", result, n-i, i);
+    if (i != n) printf(" + ");
+  }
+
+  return;
+}
+
+int commutation(int n, int r) {
+  int c = 1;
+  int R = 1;
+  int Rn = 1;
+
+  for (int i = 1; i <= n; i++) {
+    c = c*i;
+  }
+
+  for (int i = 1; i <= r; i++) {
+    R = R*i;
+  }
+  for (int i = 1; i <= (n - r); i++) {
+    Rn = Rn*i;
+  }
+  int result = c/(R*Rn);
+  return result;
+
+}
---
+++ demo.c
@@ -0,0 +1,36 @@
+#include <stdio.h> //Pre-processor command
+int main() //main function
+{
+    he think he the battery we call him elon musk pussy
+    dont send a bitch your dot thats how you get shot 
+    i dm bitches in vanish mode i do that shit a lot
+    this chopper came with a compressor
+    if i was will smith i would have slapped him with a stick
+
+    char prog;
+    char digit;
+
+    /* my first program in c*/
+    scanf("%c", digit);
+    printf("My Matric No is: 20C%d027584 \n"); //Statements & Expressions
+
+    if(d == "comp")
+    {
+       prog = "j" 
+    } else if (d == "elect" || "ice")
+
+{
+    prog = "k"
+};
+
+    printf("My MAtric No is 20%c027%d");
+
+    // for(const i = 0; i < arr.length; i++) {
+
+    // }
+
+    return 0;
+}
+
+
+//Write an algorithm to find the averag of an array of numbers
